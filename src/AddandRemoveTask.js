2f47c34257645f538cf9ba37808d3f4f294e28e5
mathsoft29 <iwuconcept@gmail.com>
2023-06-16 17:29:04 +0100

    refactored js to dynamically

---
+++ src/AddandRemoveTask.js
@@ -0,0 +1,105 @@
+const TaskListBlock = document.querySelector('.todo-block');
+
+class TodoTasks {
+  constructor(description, completed, id) {
+    this.description = description;
+    this.completed = completed;
+    this.id = id;
+  }
+
+  static displayTasks = (task) => {
+    const listItem = document.createElement('li');
+    listItem.id = task.id;
+    listItem.className = 'lists';
+    listItem.innerHTML = `
+          <div class="listLP">
+          <input type="checkbox" name="" id="${task.id}">
+          <p id="${task.id}">${task.description}</p>
+          </div>
+          
+          <div class="trash">
+          
+          <i id="pen" class="fa-solid fa-pen"></i>
+          <i id="delete" class="fa-solid fa-trash-can"></i>
+          </div>
+          `;
+    TaskListBlock.appendChild(listItem);
+  };
+
+  static loadFromLocalStorage() {
+    let Tasks;
+
+    if (localStorage.getItem('TasksInfo')) {
+      Tasks = JSON.parse(localStorage.getItem('TasksInfo'));
+    } else {
+      Tasks = [];
+    }
+    return Tasks;
+  }
+
+  static displayTasksOnPage() {
+    const Tasks = TodoTasks.loadFromLocalStorage();
+
+    Tasks.forEach((task) => {
+      TodoTasks.displayTasks(task);
+    });
+  }
+
+  static removeBookFromPage(target) {
+    if (target.classList.contains('trash')) {
+      target.parentElement.remove();
+    }
+  }
+
+  static removeFromLocalStorage(element) {
+    let k = 0;
+    const Tasks = TodoTasks.loadFromLocalStorage();
+
+    const idd = element.parentElement.id;
+    const newID = Number(idd);
+    for (let i = 0; i < Tasks.length; i += 1) {
+      if (Tasks[i].id === newID) {
+        k = i;
+        break;
+      }
+    }
+    Tasks.splice(k, 1);
+    let X = 1;
+    Tasks.forEach((task) => {
+      task.id = X;
+      X += 1;
+    });
+    localStorage.setItem('TasksInfo', JSON.stringify(Tasks));
+  }
+}
+
+export const removeItem = (e) => {
+  TodoTasks.removeBookFromPage(e.target.parentElement);
+  TodoTasks.removeFromLocalStorage(e.target.parentElement);
+};
+
+export const displayTasksOnWebPage = () => {
+  TodoTasks.displayTasksOnPage();
+};
+
+export const addItem = () => {
+  const addInput = document.querySelector('.add-input');
+
+  if (addInput.value) {
+    const complete = false;
+
+    const loadTasks = TodoTasks.loadFromLocalStorage();
+    const count = loadTasks.length + 1;
+    const newTask = new TodoTasks(addInput.value, complete, count);
+
+    loadTasks.push(newTask);
+
+    TodoTasks.displayTasks(newTask);
+    localStorage.setItem('TasksInfo', JSON.stringify(loadTasks));
+
+    // Reset input fields
+    addInput.value = '';
+  }
+};
+
+export const storageInfo = TodoTasks.loadFromLocalStorage;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,116 @@
+// import './style.css';
+
+// // const TaskListBlock = document.querySelector('.todo-block');
+
+// // create temp Object for the list tasks
+// const toDoTasks = [
+//   {
+//     description: 'Getting my project done',
+//     completed: 'true',
+//     index: 1,
+//   },
+//   {
+//     description: 'Buy Book',
+//     completed: 'false',
+//     index: 2,
+//   },
+//   {
+//     description: 'Pay power ultility Bills',
+//     completed: 'true',
+//     index: 3,
+//   },
+//   {
+//     description: 'morning routin execise',
+//     completed: 'false',
+//     index: 4,
+//   },
+//   {
+//     description: 'Study french for 1 hour',
+//     completed: 'true',
+//     index: 5,
+//   },
+// ];
+
+// // Create the task from the todoTasks object
+
+// const displayTasks = () => {
+//   toDoTasks.forEach((list) => {
+//     const listItem = document.createElement('li');
+//     listItem.innerHTML = `
+//         <div class="listLP">
+//         <input type="checkbox" name="" id="">
+//         <p class="task-to-be-done">${list.description}</p>
+//         </div>
+
+//         <div class="trash"><i class="fa-solid fa-trash-can"></i></div>
+
+//         `;
+//     // TaskListBlock.appendChild(listItem);
+//   });
+// };
+
+// // Create the task from the todoTasks object
+// displayTasks();
+
 import './style.css';
+import {
+  displayTasksOnWebPage, addItem, removeItem, storageInfo,
+} from './AddandRemoveTask.js';
 
 const TaskListBlock = document.querySelector('.todo-block');
+const addInTodo = document.querySelector('.form');
 
-// create temp Object for the list tasks
-const toDoTasks = [
-  {
-    description: 'Getting my project done',
-    completed: 'true',
-    index: 1,
-  },
-  {
-    description: 'Buy Book',
-    completed: 'false',
-    index: 2,
-  },
-  {
-    description: 'Pay power ultility Bills',
-    completed: 'true',
-    index: 3,
-  },
-  {
-    description: 'morning routin execise',
-    completed: 'false',
-    index: 4,
-  },
-  {
-    description: 'Study french for 1 hour',
-    completed: 'true',
-    index: 5,
-  },
-];
+window.addEventListener('load', () => {
+  displayTasksOnWebPage();
+});
 
-// Create the task from the todoTasks object
+addInTodo.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addItem();
+});
 
-const displayTasks = () => {
-  toDoTasks.forEach((list) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-        <div class="listLP">
-        <input type="checkbox" name="" id="">
-        <p class="task-to-be-done">${list.description}</p>
-        </div>
+TaskListBlock.addEventListener('click', (e) => {
+  if (e.target.id === 'delete') {
+    removeItem(e);
+  }
+  const checkMark = document.createElement('button');
+  const insertInput = document.createElement('input');
+  if (e.target.id === 'pen') {
+    e.target.addEventListener('click', () => {
+      insertInput.remove();
+      checkMark.remove();
+    });
 
-        <div class="trash"><i class="fa-solid fa-trash-can"></i></div>
-        
-        `;
-    TaskListBlock.appendChild(listItem);
-  });
-};
+    const top = e.target.parentElement.parentElement.children[0];
+    const targetElement = e.target.parentElement.parentElement.children[0].children[1];
 
-// Create the task from the todoTasks object
-displayTasks();
+    checkMark.className = 'checkEdit';
+    checkMark.innerHTML = '<i class="fa-solid fa-check fa-xl"></i>';
+    insertInput.placeholder = 'Edit your task';
+    insertInput.type = 'text';
+    insertInput.className = 'editInput';
+    top.appendChild(insertInput);
+    top.appendChild(checkMark);
+
+    const one = e.target.parentElement.parentElement.children[0].children[3];
+
+    if (one.className === 'checkEdit') {
+      const two = e.target.parentElement.parentElement.children[0].children[2];
+      one.onclick = function check(e) {
+        if (!two.value) {
+          const three = e.target.parentElement.parentElement.parentElement.children[0].children[1];
+          targetElement.innerHTML = three.innerHTML;
+          insertInput.remove();
+          checkMark.remove();
+        } else {
+          targetElement.innerHTML = two.value;
+          insertInput.remove();
+          checkMark.remove();
+          const Info = storageInfo();
+          Info[targetElement.id - 1].description = two.value;
+          localStorage.setItem('TasksInfo', JSON.stringify(Info));
+        }
+      };
+    }
+  }
+});
